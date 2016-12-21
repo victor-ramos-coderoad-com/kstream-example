2e6dc676ac8f73c56349dc023d589cc06c345a28
Victor Ramos <victor.ramos@mojix.com>
2016-12-21 17:21:32 -0400

    KTableCache. Example of corebridge with thing cache as KTable

---
+++ src/main/java/com/mojix/examples/KTableCache.java
@@ -0,0 +1,118 @@
+package com.mojix.examples;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+/**
+ * Created by vramos on 12/21/16.
+ */
+public class KTableCache {
+    public static void main(String[] args) throws Exception{
+        final Properties streamsConfiguration = new Properties();
+        // Application unique name in the Kafka cluster
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "kcorebridge");
+        // Kafka broker(s).
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, "kcorebridge-pipe");
+        // ZooKeeper ensemble.
+        streamsConfiguration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, "localhost:2181");
+        // Specify default (de)serializers for record keys and for record values.
+        streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        streamsConfiguration.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, "4");
+
+        final Serde<String> stringSerde = Serdes.String();
+        final Serde<Long> longSerde = Serdes.Long();
+        final Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
+
+        // INPUT
+        final KStreamBuilder builder = new KStreamBuilder();
+        final KStream<String, String> blinks = builder.stream(stringSerde, stringSerde, "___v1___data___ALEB");
+
+        final KTable<String,String> thingCache = builder.table(stringSerde,stringSerde, "___v1___cache___thing");
+
+        // TRANSFORMATIONS
+        final KStream<String, String> dataout = blinks
+                .flatMapValues(new ValueMapper<String, Iterable<String>>() {
+                    @Override
+                    public Iterable<String> apply(String value) {
+                        System.out.println("1. Creating thing message "+Thread.currentThread().getId());
+                        try {
+                            Thread.sleep(5);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return Arrays.asList(pattern.split(value.toLowerCase()));
+                    }
+                })
+                .leftJoin(thingCache, new ValueJoiner<String, String, String>() {
+                    @Override
+                    public String apply(String value1, String value2) {
+                        System.out.println(String.format(">>>> (blink,cache):(%s,%s)",value1,value2));
+                        return value1;
+                    }
+                })
+                .map(new KeyValueMapper<String, String, KeyValue<String, String>>() {
+                    @Override
+                    public KeyValue<String, String> apply(String key, String value) {
+                        System.out.println("2. Instantiating thing "+Thread.currentThread().getId());
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return new KeyValue<>(key, value);
+                    }
+                })
+                .map(new KeyValueMapper<String, String, KeyValue<String, String>>() {
+                    @Override
+                    public KeyValue<String, String> apply(String key, String value) {
+                        System.out.println("3. Processing thing message "+Thread.currentThread().getId());
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return new KeyValue<>(key, value);
+                    }
+                })
+                .map(new KeyValueMapper<String, String, KeyValue<String, String>>() {
+                    @Override
+                    public KeyValue<String, String> apply(String key, String value) {
+                        System.out.println("4. Creating json data out "+Thread.currentThread().getId());
+                        System.out.println("_______________________________________________");
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return new KeyValue<>(key, value);
+                    }
+                });
+
+//        final KStream<String, Long> dataout = blinks
+//                .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
+//                .map((key, word) -> new KeyValue<>(word, word))
+//                .through("RekeyedIntermediateTopic")
+//                .countByKey("Counts")
+//                .toStream();
+
+        // OUTPUT
+        dataout.to(stringSerde, stringSerde, "___v1___dataout");
+
+        // Starts the application
+        final KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
+        streams.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+}
